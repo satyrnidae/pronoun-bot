d469b11dfeb78b2450f6d89ba2b82e77f3099e61
Bridger Maskrey <maskreybe@outlook.com>
2019-02-15 12:29:07 -0500

    Implement main functionality 🎬 Ready for take 1! We now load commands, events, and start the client

--- index.ts
+++ index.ts
@@ -1,1 +1,76 @@
-console.log("Hello World!");
+import registeredCommands from './commands/registered-commands.json';
+import registeredEvents from './events/registered-events.json';
+import fs = require('fs');
+import i18n = require('i18n');
+import Command from './src/command.js';
+import EventBus from './src/event-bus';
+import EventHandler from './src/event-handler.js';
+import configuration from './src/configuration';
+import CommandRegistry from './src/command-registry';
+import { Client } from 'discord.js';
+import { contains } from './src/utility';
+
+i18n.configure({
+    locales: ['en'],
+    directory: `${__dirname}/locale`
+})
+
+var client = new Client();
+
+var commandRegistry = new CommandRegistry();
+var eventBus = new EventBus(() => commandRegistry.commands, client);
+
+const commandDirectory = `${__dirname}/commands`;
+fs.readdir(commandDirectory, (err, items) => {
+    if(err) {
+        return console.error(i18n.__('Unable to load commands from %s: %s', commandDirectory, `${err}`));
+    }
+    items.forEach(item => {
+        var [_,fileName,extension] = item.match(/^(.+)\.(.+)$/);
+        if(!extension.match(/^(ts|js)$/g)) {
+            return console.warn(i18n.__('Skipped command file %s as it is does not appear to be a valid module file.', item));
+        }
+        if(configuration.trustAllCommands !== true && !contains(registeredCommands, fileName[0])) {
+            return console.warn(i18n.__('Skipped command file %s as it was not specified in the registered commands list.', item));
+        }
+        try {
+            const module = require(`${commandDirectory}/${item}`);
+            const command = new module.default() as Command;
+            if(!command) return console.error(i18n.__('Failed to load command from %s', item));
+            commandRegistry.register(command);
+            return console.debug(i18n.__('Successfully registered command %s', command.name));
+        } catch (ex) {
+            return console.error(i18n.__('Failed to register command in file %s: %s', item, ex));
+        }
+    });
+});
+
+const eventDirectory = `${__dirname}/events`;
+fs.readdir(eventDirectory, (err, items) => {
+    if(err) {
+        return console.error(i18n.__('Unable to load events from %s: %s', eventDirectory, `${err}`));
+    }
+    items.forEach(item => {
+        var [_,fileName,extension] = item.match(/^(.+)\.(.+)$/);
+        if(!extension.match(/^(ts|js)$/g)) {
+            return console.warn(i18n.__('Skipped event file %s as it is does not appear to be a valid module file.', item));
+        }
+        if(configuration.trustAllEvents !== true && !contains(registeredEvents, fileName)) {
+            return console.warn(i18n.__('Skipped event file %s as it was not specified in the registered events list.', item));            
+        }
+        try {
+            const module = require(`${eventDirectory}/${item}`)
+            const event = new module.default() as EventHandler;
+            if(!event) return console.error(i18n.__('Failed to load event from %s', item));
+            eventBus.register(event);
+            return console.debug(i18n.__('Successfully registered event %s', event.name));
+        } catch (ex) {
+            return console.error(i18n.__('Failed to register event in file %s: %s', item, ex));
+        }
+    });
+});
+
+client.login(configuration.token)
+    .catch(reason => {
+        console.error(i18n.__('Failed to launch pronoun bot:'), reason)
+    });
