7472ffdc6e172dee8849107182d061a945cc913f
Bridger Maskrey <maskreybe@outlook.com>
2019-02-15 13:36:33 -0500

    Fix hearts emote 💖 getHeart() now returns the emote value (:%s:)

--- src/messages.ts
+++ src/messages.ts
@@ -3,5 +3,5 @@ import * as messages from '../config/messages.json';
 export function getHeart() {
     //TODO: Per-guild configurable heart reactions via data access
     const i = Math.floor(Math.random() * messages.hearts.length)
-    return messages.hearts[i];
+    return `:${messages.hearts[i]}:`;
 }
